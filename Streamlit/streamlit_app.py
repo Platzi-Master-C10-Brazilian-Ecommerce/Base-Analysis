5dfff711426903aea14c26d5ae69a8687d9cb8fd
Julian <juliancastro320@gmail.com>
2022-06-02 17:58:59 -0500

    Se eliminó un archivo no necesario (test)

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -6,6 +6,8 @@ import psycopg2
 from sqlalchemy import create_engine
 from PIL import Image
 
+
+#Se establece la conexión con la basse de datos remota
 host1 = 'platzicohort10@platzicohort10.cig2rbjhhqmz.us-east-1.rds.amazonaws.com'
 user1 = 'usuario_consulta'
 password1 = 'platzicohort10'
