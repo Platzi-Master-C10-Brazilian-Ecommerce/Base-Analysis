7678b180f13bd219217b2fb2acdee7a3ab0869c1
Julian <juliancastro320@gmail.com>
2022-06-24 16:05:19 -0500

    Ver si funciona el HTML de Emmanuel

--- Streamlit/Home.py
+++ Streamlit/Home.py
@@ -57,3 +57,43 @@ with H:
 
 with I: 
     I.markdown("conclusiones")
+
+#---------------------------------------------------------#
+
+J, K ,L = st.columns(3)
+
+st.header("A priori")
+
+with J:
+    pass
+
+#---------------------------------------------------------#
+
+M, O = st.columns(2)
+
+st.header("Maps")
+
+with urlopen('https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson') as response:
+    Brazil = json.load(response) # Javascrip object notation 
+
+option = st.selectbox(
+     'Display query',
+     ('Count products', 'Payments products', 'percapita'))
+
+if option =="Count products":
+    st.header("")
+    HtmlFile = open("Data-Analysis-for-Brazilian-E-commerce/Data-Analysis-Drafts/Emmanuel HTML/count.html", 'r', encoding='utf-8')
+    source_code = HtmlFile.read() 
+    components.html(source_code, height = 600, scrolling=True)
+elif option =="Payments products":  
+    st.header("") 
+    HtmlFile = open("Data-Analysis-for-Brazilian-E-commerce/Data-Analysis-Drafts/Emmanuel HTML/payment.html", 'r', encoding='utf-8')
+    source_code = HtmlFile.read() 
+    components.html(source_code, height = 600, scrolling=True)
+else:
+    st.header("")
+    HtmlFile = open("Data-Analysis-for-Brazilian-E-commerce/Data-Analysis-Drafts/Emmanuel HTML/payment.html", 'r', encoding='utf-8')
+    source_code = HtmlFile.read() 
+    components.html(source_code, height = 600, scrolling=True)
+
+
