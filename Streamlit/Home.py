4a93b779ddec1bda4e9e2d4b655a23742941daea
Julian <juliancastro320@gmail.com>
2022-06-24 15:32:52 -0500

    No se pudo el multipages en cloud

--- Streamlit/Home.py
+++ Streamlit/Home.py
@@ -5,25 +5,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#dashboard
 import streamlit as st
 from PIL import Image
 
 
-
 # Configuración de la pagina
-st.set_page_config(page_title="Bussines Intelligence Team",page_icon="🪙",layout="wide")
-st.sidebar.success("Paginas")
+st.set_page_config(page_title="Bussines Intelligence Team",page_icon="📈",layout="wide")
 
 
-a1, a2 = st.columns(2)
-a1.image(Image.open('Streamlit/pages/Images/PM_LOGO.jpg'))
-with a2:
+# Presentación de filas
+
+A, B, C = st.columns(3)
+
+A1.image(Image.open())
+
+with B:
     st.markdown('''# Platzi Master Cohort 10
     Bussines Intelligence Team
     - Julián Castro     - Ricardo Escamilla
     - Emmanuel Escobar  - Marco Rocha
     - Juan Rincon       - Robert Barrios
     ''')
-   
+
+with C:
+    st.text("MIMIM")
+
 
--- Streamlit/pages/1_📗_About_Team.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
-
-st.set_page_config(page_title="Team", page_icon="📈")
-st.sidebar.success("Select a demo above.")
