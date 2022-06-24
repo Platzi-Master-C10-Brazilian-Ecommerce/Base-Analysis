5b95989ff20fbf2c575a5531315634895df7ec2b
Julian <juliancastro320@gmail.com>
2022-06-24 15:15:57 -0500

    Cambiando el path

--- Streamlit/pages/1_📗_About_Team.py
+++ Streamlit/pages/1_📗_About_Team.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-
+st.markdown("# Page 2 ❄️")
+st.sidebar.markdown("# Page 2 ❄️")
 
 st.set_page_config(page_title="Team", page_icon="📈")
 st.sidebar.success("Select a demo above.")
